31dbc3b2d35c973f0c2205ac66467dcbb68dd39c
OGOLA-SOSPETER <captainsos483@gmail.com>
2022-11-13 21:34:08 +0300

    Added the main menu and the dropdown menu

--- app/src/main/java/com/SosDeveloper/animalapp/Dashboard.kt
+++ app/src/main/java/com/SosDeveloper/animalapp/Dashboard.kt
@@ -1,5 +1,6 @@
 package com.SosDeveloper.animalapp
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,17 +10,15 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.filled.Share
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Alignment.Companion.Top
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -29,65 +28,66 @@ import androidx.navigation.NavController
 @Composable
 fun Dashboard(navController: NavController) {
     Column(modifier = Modifier.padding(top = 0.dp)) {
+
+
+        // Create a boolean variable
+        // to store the display menu state
+        var mDisplayMenu by remember { mutableStateOf(false) }
+
+        // fetching local context
+        val mContext = LocalContext.current
+
+        // Creating a Top bar
         TopAppBar(
-            elevation = 4.dp,
-            title = {
-                Text("I'm a TopAppBar")
-            },
-            backgroundColor = MaterialTheme.colors.primarySurface,
-            navigationIcon = {
-                IconButton(onClick = {navController.navigate("Login")}) {
-                    Icon(Icons.Filled.ArrowBack, null)
+            title = { Text("DASHBOARD| Main_Page", color = Color.White) },
+            backgroundColor = Color(0xff0f9d58),
+            actions = {
+
+                // Creating Icon button favorites, on click
+                // would create a Toast message
+                IconButton(onClick = {
+                    Toast.makeText(mContext, "Favorite", Toast.LENGTH_SHORT).show()
+                }) {
+                    Icon(Icons.Default.Favorite, "")
                 }
-            }, actions = {
-                IconButton(onClick = {/* Do Something*/ }) {
-                    Icon(Icons.Filled.Share, null)
+
+                // Creating Icon button for dropdown menu
+                IconButton(onClick = { mDisplayMenu = !mDisplayMenu }) {
+                    Icon(Icons.Default.Menu, "MainMenu")
                 }
-                IconButton(onClick = {navController.navigate("settings") }) {
-                    Icon(Icons.Filled.Settings, null)
+
+                // Creating a dropdown menu
+                DropdownMenu(
+                    expanded = mDisplayMenu,
+                    onDismissRequest = { mDisplayMenu = false }
+                ) {
+
+                    // Creating dropdown menu item, on click
+                    // would create a Toast message
+                    DropdownMenuItem(onClick = {
+                        Toast.makeText(
+                            mContext,
+                            "Settings",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }) {
+                        Text(text = "Settings")
+                    }
+
+                    // Creating dropdown menu item, on click
+                    // would create a Toast message
+                    DropdownMenuItem(onClick = {
+                        Toast.makeText(
+                            mContext,
+                            "Logout",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }) {
+                        Text(text = "Logout")
+                    }
                 }
-            })
-
-        Text("Hello World")
-
-    }
-    Column (modifier = Modifier.padding(100.dp)){
-        OutlinedTextField(
-            value = "", onValueChange = {},
-            modifier = Modifier
-                .background(Color.White, CircleShape).fillMaxWidth()
-                .padding(8.dp),
-            trailingIcon = {
-                Icon(
-                    painter = painterResource(R.drawable.google),
-                    contentDescription = "Google Search"
-                )
-            },
-
-            )
-        Text(
-            text = "Welcome to my Dashboard",
-            textAlign = TextAlign.Center
-
+            }
         )
 
-        Text(text = "I love Animals")
-
-
-        Button(onClick = { navController.navigate("Images") }) {
-            Text(
-                "MyImages",
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(top = 10.dp, bottom = 1.dp)
-            )
-
-            Icon(
-                painter = painterResource(R.drawable.google),
-                contentDescription = "My Icon",
-                modifier = Modifier.align(Top)
-                    .size(70.dp)
-                    .clip(RoundedCornerShape(16.dp))
-            )
-        }
     }
 }
